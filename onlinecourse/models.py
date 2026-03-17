e778ad9d39bb042c84a7fdbc7073fb92851b8551
segun-ade <80776153+segun-ade@users.noreply.github.com>
2026-03-18 01:12:11 +0300

    Update models.py

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -94,14 +94,6 @@ class Enrollment(models.Model):
     mode = models.CharField(max_length=5, choices=COURSE_MODES, default=AUDIT)
     rating = models.FloatField(default=5.0)
 
-
-# One enrollment could have multiple submission
-# One submission could have multiple choices
-# One choice could belong to multiple submissions
-#class Submission(models.Model):
-#    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    choices = models.ManyToManyField(Choice)
-
 class Question(models.Model): 
     course = models.ForeignKey(Course, on_delete=models.CASCADE) 
     content = models.CharField(max_length=200) 
